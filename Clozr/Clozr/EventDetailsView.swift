2e04ac32a2e27aa0200354981c01ae16cc2cb12e
Fateh Singh <fsingh@locals-MacBook-Pro-2.local>
2017-04-30 23:21:03 -0700

    Finished most of the details screen.
    ToDo -
    - Finish the chat section of details screen.
    - Finish the chat and maps view controllers.

---
+++ Clozr/Clozr/EventDetailsView.swift
@@ -0,0 +1,63 @@
+//
+//  EventDetailsView.swift
+//  Clozr
+//
+//  Created by Fateh Singh on 4/30/17.
+//  Copyright © 2017 Faback. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class EventDetailsView: UIView, UICollectionViewDataSource, UICollectionViewDelegate, MKMapViewDelegate {
+
+    @IBOutlet weak var friendListCollectionView: UICollectionView!
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var smallMapView: MKMapView!
+    @IBOutlet weak var smallChatView: UIView!
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initSubView()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initSubView()
+    }
+    
+    func initSubView() {
+        let nib = UINib(nibName: "EventDetails", bundle: nil)
+        nib.instantiate(withOwner: self, options: nil)
+        contentView.frame =  bounds
+        addSubview(contentView)
+        
+        friendListCollectionView.dataSource = self
+        friendListCollectionView.delegate   = self
+        
+        let friendListCellNib = UINib(nibName: "FriendListCell", bundle: nil)
+        friendListCollectionView.register(friendListCellNib, forCellWithReuseIdentifier: "FriendListCell")
+        
+        smallMapView.delegate = self
+        
+        // Set the location of the event here.
+        let mapCenter = CLLocationCoordinate2D(latitude: 37.3549144, longitude: -122.0035661)
+        let mapSpan = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: mapCenter, span: mapSpan)
+        // Set animated property to true to animate the transition to the region
+        smallMapView.setRegion(region, animated: false)
+        print(smallChatView.frame.size)
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = friendListCollectionView.dequeueReusableCell(withReuseIdentifier: "FriendListCell", for: indexPath) as! FriendListCellCollectionViewCell
+        return cell
+    }
+
+}
---
+++ Clozr/Clozr/FriendListCellCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  FriendListCellCollectionViewCell.swift
+//  Clozr
+//
+//  Created by Fateh Singh on 4/30/17.
+//  Copyright © 2017 Faback. All rights reserved.
+//
+
+import UIKit
+
+class FriendListCellCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var friendImage: UIImageView!
+    @IBOutlet weak var ResponseImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
